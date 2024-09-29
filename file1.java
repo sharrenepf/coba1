5037c6c8857eea84f17760a6d6755b318cc8163b
rifat djibran <rifatdjibran099@gmail.com>
2024-09-29 21:35:18 +0700

    cobacoba

---
+++ file1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class file1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Masukkan sebuah angka:");
+        int angka = input.nextInt();
+        
+    }
+}
